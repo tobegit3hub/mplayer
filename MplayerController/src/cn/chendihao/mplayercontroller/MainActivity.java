51edf7270a656971e438ae7f33bd5b02a198602f
tobe <tobegit3hub@gmail.com>
2014-04-12 02:19:47 +0800

    update code formate in MplayerController

--- MplayerController/src/cn/chendihao/mplayercontroller/MainActivity.java
+++ MplayerController/src/cn/chendihao/mplayercontroller/MainActivity.java
@@ -29,104 +29,86 @@ public class MainActivity extends Activity {
 	
 	private String command;
 	
-
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
 
 		activity = this;
 
 		commandText = (EditText) findViewById(R.id.command_text);
 		runButton = (ImageButton) findViewById(R.id.run_button);
 		
-		
 		runButton.setOnClickListener(new OnClickListener() {
 
-			@Override
-			public void onClick(View arg0) {
+        @Override
+          public void onClick(View arg0) {
 				
-				String commandContent = commandText.getText().toString().trim().toLowerCase();
+          String commandContent = commandText.getText().toString().trim().toLowerCase();
 
-				if (commandContent.startsWith("play") || commandContent.startsWith("stop") ) {
-					command = commandContent;
-					sendCommand(command);
-				}else{
-					Toast.makeText(activity, "Invalid Command", Toast.LENGTH_SHORT).show();
-				}
+          if (commandContent.startsWith("play") || commandContent.startsWith("stop") ) {
+            command = commandContent;
+            sendCommand(command);
+          }else{
+            Toast.makeText(activity, "Invalid Command", Toast.LENGTH_SHORT).show();
+          }
 
-			}
+        }
 
-		});
+      });
 		
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		
 		for (int i = 0; i < menu.size(); i++) {
 			menu.getItem(i).setOnMenuItemClickListener(
-					new OnMenuItemClickListener() {
+        new OnMenuItemClickListener() {
 
-						@Override
-						public boolean onMenuItemClick(MenuItem item) {
-							switch (item.getItemId()) {
-							
-							case R.id.action_about:
-
-								Toast.makeText(
-										activity,
-										"Developer:\n\n\n\t吴健松\n\n\t陈迪豪\n\n\t杨伟宏\n\n\t陈远龙\n\n\t文力",
-										Toast.LENGTH_LONG).show();
-
-								break;
-
-							case R.id.action_play:
-
-								command = "play ipad2.mp4";
-								sendCommand(command);
-								
-								break;
-								
-							case R.id.action_stop:
-								
-								command = "stop";		
-								sendCommand(command);
-								
-								break;
-								
-							}
-							return false;
-							
-						}
-						
-					});
+          @Override
+            public boolean onMenuItemClick(MenuItem item) {
+            switch (item.getItemId()) {
+            case R.id.action_about:
+              Toast.makeText(
+                activity,
+                "Developer:\n\n\n\t吴健松\n\n\t陈迪豪\n\n\t杨伟宏\n\n\t陈远龙\n\n\t文力",
+                Toast.LENGTH_LONG).show();
+              break;
+            case R.id.action_play:
+              command = "play ipad2.mp4";
+              sendCommand(command);
+              break;
+            case R.id.action_stop:
+              command = "stop";		
+              sendCommand(command);
+              break;
+            }
+            return false;
+          }
+        });
 		
 		}
 		
 		return true;
 	}
 
-	
 	@Override
-	public void onBackPressed() {
+    public void onBackPressed() {
 		new AlertDialog.Builder(this)
-				.setTitle("Exit Mplayer Controller?")
-				.setIcon(R.drawable.ic_launcher)
-				.setPositiveButton("Yes",
-						new DialogInterface.OnClickListener() {
-							@Override
-							public void onClick(DialogInterface dialog,
-									int which) {
-
-								System.exit(0);
-							}
-						}).setNegativeButton("Cancel", null).show();
+      .setTitle("Exit Mplayer Controller?")
+      .setIcon(R.drawable.ic_launcher)
+      .setPositiveButton("Yes",
+                         new DialogInterface.OnClickListener() {
+                           @Override
+                             public void onClick(DialogInterface dialog,
+                                                 int which) {
+                             System.exit(0);
+                           }
+                         }).setNegativeButton("Cancel", null).show();
 	}
-	
 	
 	void sendCommand(String command){
 		try {
@@ -136,8 +118,8 @@ public class MainActivity extends Activity {
 			socket = new Socket(SERVER_IP, SERVER_PORT);
 
 			PrintWriter out = new PrintWriter(new BufferedWriter(
-					new OutputStreamWriter(socket.getOutputStream())),
-					true);
+        new OutputStreamWriter(socket.getOutputStream())),
+        true);
 
 			out.print(command);
 			
@@ -151,6 +133,5 @@ public class MainActivity extends Activity {
 			Toast.makeText(activity, "Socket Exception", Toast.LENGTH_SHORT).show();
 		}
 	}
-	
 	
 }
